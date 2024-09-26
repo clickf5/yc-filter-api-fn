ca9b3ab77ce96a5234ad371bbab89b43b32ead72
Eugene Panin <paninhome@gmail.com>
2024-09-26 14:10:14 +0300

    fix: use own types

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,49 @@
-import { Handler } from '@yandex-cloud/function-types';
 import axios from 'axios';
 
-export const handler = async (data: any, context: any) => {
+type HttpMethod = 'OPTIONS' | 'HEAD' | 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+
+type RequestContext = {
+	identity: {
+		sourceIp: string;
+		userAgent: string;
+	};
+	httpMethod: HttpMethod;
+	requestId: string;
+	requestTime: string;
+	requestTimeEpoch: number;
+};
+
+interface Event {
+	httpMethod: HttpMethod;
+	headers: Record<string, string>;
+	multiValueHeaders: Record<string, string[]>;
+	queryStringParameters: Record<string, string>;
+	multiValueQueryStringParameters: Record<string, string[]>;
+	requestContext: RequestContext & {
+		apiGateway?: {
+			operationContext: {
+				include: string[];
+			};
+		};
+	};
+	body: string;
+	isBase64Encoded: boolean;
+	url: string;
+}
+
+interface Result {
+	statusCode: number;
+	headers?: Record<string, string>;
+	multiValueHeaders?: Record<string, string[]>;
+	body?: string;
+	isBase64Encoded?: boolean;
+}
+
+type HttpHandler = (event: Event) => Promise<Result>;
+
+export const handler: HttpHandler = async (data) => {
 	return {
 		statusCode: 200,
-		body: {
-			data: JSON.stringify(data),
-			context: JSON.stringify(context),
-		},
+		body: JSON.stringify(data),
 	};
 };
