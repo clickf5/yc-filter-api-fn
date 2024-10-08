d47673fbd7abb6a11b9cd05480b2fcde5c57f810
Eugene Panin <paninhome@gmail.com>
2024-10-09 00:16:44 +0300

    fix: debug multipart/form-data

--- src/index.ts
+++ src/index.ts
@@ -113,7 +113,7 @@ export const handler: HttpHandler = async (data) => {
 		for (let i = 0; i < parts.length; i++) {
 			console.log('part', i, ': ', JSON.stringify(parts[i]));
 			if (Object.hasOwn(parts[i], 'filename')) {
-				formData.append(parts[i].name ?? 'files', parts[i].data.toString());
+				formData.append(parts[i].name ?? 'files', parts[i].data, parts[i].filename);
 			} else {
 				formData.append(parts[i].name ?? 'field', parts[i].data.toString());
 			}
