a1e7d8dc0488d3daf204a6e51c4ce0dc628d7f67
Eugene Panin <paninhome@gmail.com>
2026-02-18 15:22:00 +0300

    refactor: update ESLint configuration for improved readability and consistency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,37 +1,36 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default [
-  {
-    files: ["**/*.{js,mjs,cjs,ts}"]
-  },
-  {
-    languageOptions: { globals: globals.node }
-  },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  eslintPluginPrettierRecommended,
-  {
-    rules: {
-      "@typescript-eslint/ban-types": "off",
-      "@typescript-eslint/no-unused-vars": ["off"],
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/explicit-function-return-type": ["warn"],
-      "prettier/prettier": [
-        "error",
-        {
-          "singleQuote": true,
-          "useTabs": true,
-          "semi": true,
-          "trailingComma": "all",
-          "bracketSpacing": true,
-          "printWidth": 100,
-          "endOfLine": "auto"
-        }
-      ]
-    }
-  }
+	{
+		files: ['**/*.{js,mjs,cjs,ts}'],
+	},
+	{
+		languageOptions: { globals: globals.node },
+	},
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	eslintPluginPrettierRecommended,
+	{
+		rules: {
+			'@typescript-eslint/ban-types': 'off',
+			'@typescript-eslint/no-unused-vars': ['off'],
+			'@typescript-eslint/no-explicit-any': 'off',
+			'@typescript-eslint/explicit-function-return-type': ['warn'],
+			'prettier/prettier': [
+				'error',
+				{
+					singleQuote: true,
+					useTabs: true,
+					semi: true,
+					trailingComma: 'all',
+					bracketSpacing: true,
+					printWidth: 100,
+					endOfLine: 'auto',
+				},
+			],
+		},
+	},
 ];
